b07bb7e3ac562398fb4a65410c5191672a0a2740
rustam iskandarov <rustpo4ta@namil.rugit config --global user.name rustam>
2022-02-08 17:47:24 +0300

    add addRoleForUser in controller and service

--- src/exeptions-consts.ts
+++ src/exeptions-consts.ts
@@ -1,5 +1,7 @@
 export const EMAIL_IS_BUSY_ERROR = 'Данный еmail уже используеться';
+export const USERNAME_IS_BUSY_ERROR = 'Данный username уже используеться';
 export const USER_NO_EXISTS_ERROR = 'Пользователь с данным email не существует';
+export const USER_NOT_FOUND_ERROR = 'Пользователь не найден';
 export const USER_UNAUTHORIZED_ERROR = 'Пользователь не авторизован';
 export const WRONG_PASSWORD_ERROR = 'Не верный пароль пользователя';
 export const WRONG_LOGIN_AND_PASSWORD_ERROR = 'Не верный логин или пароль пользователя';
